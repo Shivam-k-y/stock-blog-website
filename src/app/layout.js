94df80737d1b4f2ff8be9398ba493f942bb7576c
Shivam-k-y <shivamky1729@gmail.com>
2026-06-06 01:07:47 +0530

    Integrate Google Analytics and update dependencies: add @next/third-parties package for Google Analytics support in layout.js, enhancing tracking capabilities. Update package.json and package-lock.json to include the new dependency.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer"
 import WhatsAppButton from "@/components/WhatsAppButton"
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,7 +70,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children} <Footer /> <WhatsAppButton /> </body>
+      <body className="min-h-full flex flex-col">{children} <Footer /> <WhatsAppButton /> 
+      <GoogleAnalytics gaId="G-QWGDZ8P6YJ" />
+      </body>
     </html>
   );
 }
